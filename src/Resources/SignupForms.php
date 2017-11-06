0aa61d0777c532574a28280cb0d3e6d6d4840efd
Philip Harvey <pmh1@mac.com>
2017-11-06 11:22:06 -0800

    Add Signup Forms Resource

--- src/Resources/SignupForms.php
+++ src/Resources/SignupForms.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Emma\Resources;
+use Emma\Resources\SignupForms;
+use Emma\Http\Response;
 
 /**
  * Signup Forms resources
@@ -12,5 +14,18 @@ namespace Emma\Resources;
  */
 class SignupForms extends Resource
 {
-
+  /**
+   * Get all Signup SignupForms
+   * @param array $params Optional expected paramaters are ['count', 'start', 'end']
+   * @return Http\Response
+   */
+  public function all($params = [])
+  {
+    $options = [];
+    if (count($params) > 0)
+    {
+      $options['query'] = $params;
+    }
+    return $this->client->request('get', 'signup_forms', $options);
+  }
 }
